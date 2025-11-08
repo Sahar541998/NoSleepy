a12f8b11bce34b7f35f953f3c32cf2d6dbd8add2
saharlevy <le.sahar@gmail.com>
2025-11-08 19:36:27 +0200

    Initial Commit

---
+++ NoSleepy/NoSleepyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSleepyApp.swift
+//  NoSleepy
+//
+//  Created by Sahar Levy on 08/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoSleepyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
